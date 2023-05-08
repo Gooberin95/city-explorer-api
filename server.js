09c5fcbbaae6e2afee085776b7826232051e4490
Brandon Ruiz <Sober4202020@outlook.com>
2023-05-08 03:55:31 +0000

    cleaning up to get closer

--- server.js
+++ server.js
@@ -21,28 +21,30 @@ app.get('/weather', (request, response) => {
     let dates = weather.map( (val ) => val.data[0].datetime);
     let descr = weather.map((val ) => val.data[0].weather);
     console.log(`Seattle is at index ${y}`);
-    console.log(descr);
+    console.log(dates);
     
 
-    response.status(200).send(dates);
+    response.status(200).send(descr);
   }
-  else if (searchQuery === 'Paris') {
+  else if (searchQuery === 'Paris' && lat === '2.3200410217200766' && lon === '48.8588897') {
     let a = weather.find(val => val.city_name === searchQuery);
     let b = weather.findIndex(val => val.city_name === searchQuery);
-    let names = weather.map( (val ) => val.data[1].datetime);
+    let day = weather.map( (val ) => val.data[1].datetime);
+    let condit = weather.map( (val) => val.data[1].weather);
     console.log(`Paris is at index ${b}`);
-    console.log(names);
+    console.log(day);
 
-    response.status(200).send(names);
+    response.status(200).send(condit);
   }
-  else if (searchQuery === 'Amman') {
+  else if (searchQuery === 'Amman' && lat === '35.9239625' && lon === '31.9515694') {
     let one = weather.find(val => val.city_name === searchQuery);
     let two = weather.findIndex(val => val.city_name === searchQuery);
-    let names = weather.map( (val ) => val.data[2].datetime);
+    let dayOf = weather.map( (val ) => val.data[2].datetime);
+    let weath = weather.map((val ) => val.data[2].weather);
     console.log(`Amman is at index ${two}`);
-    console.log(names);
+    console.log(dayOf);
 
-    response.status(200).send(names);
+    response.status(200).send(weath);
   }
 
   return response.status(200).send('Error');
