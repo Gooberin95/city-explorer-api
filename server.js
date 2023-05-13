a6075a92892a1d203bbebcce6a38b7d1be8ea543
Brandon Ruiz <Sober4202020@outlook.com>
2023-05-13 04:00:17 +0000

    saving progress before i accidentally loose it

--- server.js
+++ server.js
@@ -9,13 +9,28 @@ app.use(cors());
 
 const PORT = process.env.PORT;
 const WEATHER_BIT = process.env.WEATHER_BIT;
-
+const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 // app.get('/', (request, response) => {
 //   response.status(200).send(weather);
 // });
 
+
+app.get('/movies', async (request, response) => {
+  const {searchQuery} = request.query;
+  const API = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&q=${this.state.searchQuery}&format=json`;
+  
+  const movieRes = await axios.get(API);
+  console.log('Here is the url you searched:....', API);
+  console.log(movieRes.data);
+  this.setState({ videoStats: movieRes.data[0] });
+
+  console.log(`${this.state.videoStats}`);
+  
+}
+);
+
 app.get('/weather',async (request, response) => {
-  const { lat, lon} = request.query;
+  const { lat, lon,} = request.query;
 
   const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${WEATHER_BIT}`;
   console.log('This is the url', url);
@@ -23,30 +38,10 @@ app.get('/weather',async (request, response) => {
   let formattedData = weatherResponse.data.data.map( val => new Forecast(val));
   
   console.log('this is weather response', formattedData);
-  // if (searchQuery === 'Seattle' && lat === '-122.330062' && lon === '47.6038321') {
-  //   let x = url.find(val => val.city_name === searchQuery);
-  //   let read = x.data.map(val => new Forecast(val));
-  //   response.status(200).send(read);
-  // }
-  // else if (searchQuery === 'Paris' && lat === '2.3200410217200766' && lon === '48.8588897') {
-  //   let a = url.find(val => val.city_name === searchQuery);
-  //   let data = a.data.map( val => new Forecast(val));
-  //   response.status(200).send(data);
-  // }
-  // else if (searchQuery === 'Amman' && lat === '35.9239625' && lon === '31.9515694') {
-  //   let one = url.find(val => val.city_name === searchQuery);
-  //   let two = url.findIndex(val => val.city_name === searchQuery);
-  //   let dayOf = one.data.map( val => new Forecast(val));
-  //   // let weath = weather.map((val ) => val.data[2].weather);
-  //   console.log(`Amman is at index ${two}`);
-
-  //   console.log(dayOf);
-  //   console.log(one);
-
-  //   response.status(200).send(dayOf);
-  // }
+  
 
   return response.status(200).send(formattedData);
+  
 }
 );
 
@@ -55,6 +50,20 @@ class Forecast {
     this.description = obj.weather.description;
     this.temp = obj.temp;
     this.date = obj.datetime;
+  }
+}
+
+class Movies {
+  constructor(obj) {
+    this.title = obj.title;
+    this.overview = obj.overview;
+    this.average_votes = obj.vote_average;
+    this.total_votes = obj.vote_count;
+    this.image_url = obj.poster_path;
+    this.popularity = obj.popularity;
+    this.released_on = obj.release_date;
+
+
   }
 }
 
