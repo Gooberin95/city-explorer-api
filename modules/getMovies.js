1cc5dde0b5f5cdd14caede32e466e9d2c2514c49
Brandon Ruiz <Sober4202020@outlook.com>
2023-05-18 05:49:49 +0000

    got caching to work

--- modules/getMovies.js
+++ modules/getMovies.js
@@ -1,22 +1,43 @@
 'use strict';
 const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 const axios = require('axios');
+const cache = require('./cache');
+
+
+
 
 function getMovies(request, response) {
+
   const {searchQuery} = request.query;
   const API = `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&api_key=${MOVIE_API_KEY}&format=json`;
-  axios
-    .get(API)
-    .then(res => {
-      console.log(res.data);
-      const moviesFormatted = res.data.results.map(val => new Movies(val));
-      response.status(200).send(moviesFormatted);
+  const key = 'Film' + searchQuery;
 
-    })
-    .catch(err => {
-      console.error('error from super agent', err);
-      response.status(500).send(`server error ${err}`);
-    });
+  if(cache[key]) {
+    console.log("you have hit your cache");
+    response.status(200).send(cache[key].data);
+  }
+
+  else {
+    console.log("You have missed your cache");
+    axios.get(API)
+      .then(res => {
+        console.log(res.data);
+        const moviesFormatted = res.data.results.map(val => new Movies(val));
+        cache[key] = {};
+        cache[key].data = moviesFormatted;
+        response.status(200).send(cache[key].data);
+
+      })
+      .catch(err => {
+        console.error('error from super agent', err);
+        response.status(500).send(`server error ${err}`);
+      });
+
+  }
+
+
+
+
 }
 class Movies {
   constructor(obj) {
